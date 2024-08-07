4803ce0dc7a9b5751d74806aa8b8298115779612
vibu99patel <vaibhavi99patel@gmail.com>
2024-08-06 23:01:23 -0500

    me

---
+++ multiple_varible.py
@@ -0,0 +1,18 @@
+num = 5
+print(id(num))
+
+name = 'navin'
+print(id(name))
+
+a=10
+b=a 
+print(a, b)
+
+print(id(a), id(b)) 
+
+#constant value written in capitals 
+PI = 3.14
+print(PI)
+
+print(type(PI))
+
